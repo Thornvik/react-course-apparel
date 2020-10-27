7b315f56eaaa64f74433fc333c031aba8d964fca
Ture Thörnvik <tthornvik@deloittedigital.se>
2020-10-27 13:58:46 +0100

    shop page, item collection and items

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = () =>  (
-  <div>
-    <h1>hats</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route route="/hats" component={HatsPage}></Route>
+        <Route route="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
