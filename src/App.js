f7d7ddc3042defd9994b99316ba77b2781e26a69
Ture Thörnvik <tthornvik@deloittedigital.se>
2020-10-27 11:11:57 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+const HatsPage = () =>  (
+  <div>
+    <h1>hats</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route route="/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
