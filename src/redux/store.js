9fb46f23db569ece6157b86e61d17f538bacaab1
Ture Thörnvik <tthornvik@deloittedigital.se>
2020-11-03 20:51:07 +0100

    adding redux saga, shop data fetch call now done with sagas.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
+import rootSaga from "./root.saga";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   //if we are on a localhost or on a development enviornment add the logger to the middleware array
@@ -13,6 +16,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 //a persisted version of our store
