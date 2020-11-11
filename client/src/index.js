b774c9f19c0ebe765a556edaddf5cf7b7c79760a
Ture Thörnvik <tthornvik@deloittedigital.se>
2020-11-11 16:17:33 +0100

    service worker added for PWA

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux"; //a component class from redux that once passed the store will give context to the rest of the application
 import { PersistGate } from "redux-persist/integration/react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import "./index.css";
 import App from "./App";
@@ -21,3 +22,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
